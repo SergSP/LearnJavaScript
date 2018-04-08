7aa795318f91d57cb17e1fba04436c3106eec4f1
SergSP <>
2018-04-08 15:30:16 +0300

    +mark cell around

--- BattleShip/battleship.js
+++ BattleShip/battleship.js
@@ -2,15 +2,6 @@
 //     this.setAttribute('class','hit');
 // });
 
-let tdId;
-
-tdId = document.querySelectorAll('#tBoard td');
-[].forEach.call(tdId, function(td) {
-    td.addEventListener("click", function() {
-        controller.processGuess(td.id);
-    });
-});
-
 let controller = {
     guesses: 0,
 
@@ -43,14 +34,15 @@ let view = {
 
 let model = {
     boardSize: 7,
-    numShips: 4,
-    shipLength: 3,
+    numShips: 5,
+    shipLength: 2,
     shipSunk: 0,
 
     ships: [],
     guessList: [],
     fire: function (guess) {
-        if(this.guessList.indexOf(guess)>=0) return false;
+        if(this.guessList.indexOf(guess) >= 0) return false;
+        this.guessList.push(guess);
         for (let i = 0; i < this.numShips; i++) {
             let ship = this.ships[i];
             let index = ship.locations.indexOf(guess);
@@ -61,6 +53,7 @@ let model = {
                 view.displayMessage('Попадание!');
                 if (this.isSunk(ship)) {
                     this.shipSunk++;
+                    this.markAround(ship.locations);
                     view.displayMessage('Корабль потоплен!!!');
                 }
                 return true;
@@ -68,7 +61,6 @@ let model = {
         }
         view.displayMiss(guess);
         view.displayMessage('Промах!');
-        this.guessList.push(guess);
         return false;
     },
     isSunk: function (ship) {
@@ -81,7 +73,7 @@ let model = {
     },
     generateShip: function () {
         let locations = [];
-        let startPos = [Math.floor(Math.random() * (this.boardSize - this.shipLength)), Math.floor(Math.random() * (this.boardSize - this.shipLength))];
+        let startPos = [Math.floor(Math.random() * (this.boardSize - this.shipLength + 1)), Math.floor(Math.random() * (this.boardSize - this.shipLength + 1))];
         if (Math.random() < 0.5)
             for (let i = 0; i < this.shipLength; i++) locations.push(startPos[0].toString() + (startPos[1] + i).toString());
         else
@@ -101,15 +93,55 @@ let model = {
     },
     collisionShips(locations) {
         if(this.ships.length===0) return false;
+
+        let around = this.aroundCells(locations);
+
         for (let i = 0; i < this.ships.length; i++) {
             let ship = this.ships[i];
-            for (let j = 0; j < locations.length; j++) {
-                if (ship.locations.indexOf(locations[j]) >= 0) return true;
+            for (let j = 0; j < around.length; j++) {
+                if (ship.locations.indexOf(around[j]) >= 0) return true;
             }
         }
         return false;
     },
+    aroundCells: function (locations) {
+        let result=[];
+
+        for(let i=0;i<locations.length;i++){
+            for(let j=-1;j<=1;j++){
+                for(let k=-1;k<=1;k++){
+                    let x,y;
+                    let calcPos;
+                    x=Number(locations[i][0])+j;
+                    y=Number(locations[i][1])+k;
+                    calcPos=x.toString()+y.toString();
+
+                    if(calcPos.search('^[0-'+(model.boardSize-1)+']{2}$') !== -1)
+                        result.push(calcPos);
+
+                }
+            }
+        }
+        return result;
+    },
+    markAround: function (locations) {
+        let aroundCells=this.aroundCells(locations);
+        for(let i=0;i<aroundCells.length;i++){
+            if(this.guessList.indexOf(aroundCells[i]) < 0) {
+                view.displayMiss(aroundCells[i]);
+                this.guessList.push(aroundCells[i]);
+            }
+        }
+    }
 };
 window.onload = function () {
+    let tdId;
+
+    tdId = document.querySelectorAll('#tBoard td');
+    [].forEach.call(tdId, function(td) {
+        td.addEventListener("click", function() {
+            controller.processGuess(td.id);
+        });
+    });
     model.generateShipLocations();
 };
